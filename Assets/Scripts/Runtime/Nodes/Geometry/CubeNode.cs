8874dc03730fc8b615f062a5f07cbf75f4e70951
Bakso-B <bakarybayo11@gmail.com>
2023-01-18 14:18:10 +0000

    cube node update

--- Assets/Scripts/Runtime/Nodes/Geometry/CubeNode.cs
+++ Assets/Scripts/Runtime/Nodes/Geometry/CubeNode.cs
@@ -35,8 +35,61 @@ namespace MiniDini.Nodes
 
             m_geometry.Empty();
 
-            // here is where we construct the geometry for a cube
+            // here is where we construct the geometry for a cube (8 points, 6 prims, 8 indices)
 
+            Point a = new();
+            a.position = editplane.up * size;
+            Point b = new();
+            b.position = editplane.right * size;
+            Point c = new();
+            c.position = editplane.down * size;
+            Point d = new();
+            d.position = editplane.left * size;
+            Point e = new();
+            e.position = (editplane.normal) * (editplane.up * size);
+            Point f = new();
+            f.position = (editplane.normal)*(editplane.right * size);
+            Point g = new();
+            g.position = (editplane.normal)*(editplane.down * size);
+            Point h = new();
+            h.position = (editplane.normal)*(editplane.left * size);
+
+            int index1 = m_geometry.AddPoint(a);
+            int index2 = m_geometry.AddPoint(b);
+            int index3 = m_geometry.AddPoint(c);
+            int index4 = m_geometry.AddPoint(d);
+            int index5 = m_geometry.AddPoint(e);
+            int index6 = m_geometry.AddPoint(f);
+            int index7 = m_geometry.AddPoint(g);
+            int index8 = m_geometry.AddPoint(h);
+            
+            Prim first = new Prim();
+            first.points.Add(index1);
+            first.points.Add(index2);
+            first.points.Add(index3);
+            first.points.Add(index4);
+
+            m_geometry.AddPrim(first);
+
+            Prim second = new Prim();
+            second.points.Add({index5, index8, index7, index6});
+            m_geometry.AddPrim(second);
+
+            Prim third = new Prim();
+            third.points.Add({index1, index5, index6, index2});
+            m_geometry.AddPrim(third);
+
+            Prim fourth = new Prim();
+            fourth.points.Add({index4, index3, index7, index8});
+            m_geometry.AddPrim(fourth);
+
+            Prim fifth = new Prim();
+            fifth.points.Add({index1, index4, index8, index5});
+            m_geometry.AddPrim(fifth);
+
+            Prim sixth = new Prim();
+            sixth.points.Add({index2, index6, index7, index3});
+            m_geometry.AddPrim(sixth);
 
             return m_geometry;
         }
